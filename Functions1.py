96344ecfbe25945a1cbc6486b26ca529c96669a1
ST <thiagarajan.saravanan@gmail.com>
2020-11-25 20:45:04 -0800

    Where am I

--- Functions1.py
+++ Functions1.py
@@ -7,19 +7,5 @@ Created on Tue Feb  9 01:05:17 2016
 #!/usr/bin/python
 
 # Function definition is here
-def changeme( mylist ):
-   "This changes a passed list into this function"
-   mylist.append([1,2,3,4]);
-   print("Values inside the function: ", mylist)
-   return mylist
 
-# Now you can call changeme function
-mylist = [10,20,30];
-s=changeme( mylist );
-print("Values outside the function: ", mylist)
-print(s)
-# Checking the branch commit
-# 
-for i in range(1,10):
-    print(i)
 
